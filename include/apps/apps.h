8193cbc7fb4c986bc63610a918e2eada17c9803b
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2025-09-02 12:43:40 +0700

    feat: apps and main added

---
+++ include/apps/apps.h
@@ -0,0 +1,11 @@
+#ifndef __APPS_APPS_H
+#define __APPS_APPS_H
+
+#include "core/core.h"
+
+namespace apps
+{
+    void run();
+}
+
+#endif
---
+++ include/main.h
@@ -0,0 +1,9 @@
+#ifndef __MAIH_H
+#define __MAIN_H
+
+#include "apps/apps.h"
+
+void setup();
+void loop();
+
+#endif
